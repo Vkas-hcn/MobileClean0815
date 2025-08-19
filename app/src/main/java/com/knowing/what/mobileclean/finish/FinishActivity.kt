f6cc8212e637802fc7baf2e7739dc724e07ad1f7
vkas <vkas@qq.com>
2025-08-19 11:23:00 +0800

    feat: add finish scan

--- app/src/main/java/com/knowing/what/mobileclean/finish/FinishActivity.kt
+++ app/src/main/java/com/knowing/what/mobileclean/finish/FinishActivity.kt
@@ -3,6 +3,7 @@ package com.knowing.what.mobileclean.finish
 import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,12 +14,14 @@ import com.knowing.what.mobileclean.big.BigFileListActivity
 import com.knowing.what.mobileclean.databinding.ActivityFinishBinding
 import com.knowing.what.mobileclean.junk.FileUtils
 import com.knowing.what.mobileclean.junk.JunkActivity
+import com.knowing.what.mobileclean.main.NavigationAction
 
 class FinishActivity : AppCompatActivity() {
     private val binding by lazy { ActivityFinishBinding.inflate(layoutInflater) }
 
     private var countdownTimer: CountDownTimer? = null
     var jumpType = ""
+    private var scanCountDownTimer: CountDownTimer? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -45,12 +48,12 @@ class FinishActivity : AppCompatActivity() {
 
         }
         binding.atvPicture.setOnClickListener {
-            startActivity(Intent(this, ImageListActivity::class.java))
-            finish()
+            showScanDialogAndNavigate(NavigationAction.ImageClean)
+
         }
         binding.atvFile.setOnClickListener {
-            startActivity(Intent(this, BigFileListActivity::class.java))
-            finish()
+            showScanDialogAndNavigate(NavigationAction.FileClean)
+
         }
         binding.atvClean.setOnClickListener {
             startActivity(Intent(this, JunkActivity::class.java))
@@ -58,7 +61,47 @@ class FinishActivity : AppCompatActivity() {
         }
 
     }
+    private fun showScanDialogAndNavigate(action: NavigationAction) {
 
+        val iamgeData = when (action) {
+            NavigationAction.ImageClean -> {
+                R.drawable.ic_img_logo
+            }
+            NavigationAction.FileClean -> {
+                R.drawable.ic_file_logo
+            }
+            else -> {
+                R.drawable.ic_app_logo
+            }
+        }
+        binding.dialogClean.imgLogo.setImageResource(iamgeData)
+        binding.dialogClean.conClean.visibility = View.VISIBLE
+        binding.dialogClean.tvTip.text = "Scaning"
+
+        scanCountDownTimer?.cancel()
+        scanCountDownTimer = object : CountDownTimer(1000, 10) {
+            override fun onTick(millisUntilFinished: Long) {
+                val progress = ((1000 - millisUntilFinished) / 10).toInt()
+                binding.dialogClean.pg.progress = progress
+            }
+
+            override fun onFinish() {
+                binding.dialogClean.conClean.visibility = View.GONE
+                when (action) {
+                    NavigationAction.ImageClean -> {
+                        startActivity(Intent(this@FinishActivity, ImageListActivity::class.java))
+                    }
+                    NavigationAction.FileClean -> {
+                        startActivity(Intent(this@FinishActivity, BigFileListActivity::class.java))
+                    }
+                    else -> {
+                        startActivity(Intent(this@FinishActivity, BigFileListActivity::class.java))
+                    }
+                }
+                finish()
+            }
+        }.start()
+    }
     private fun showCleaningDialog() {
         val iamgeData =  if(jumpType == "image"){
             R.drawable.ic_img_logo
